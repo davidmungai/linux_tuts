d74f660d7b19e10bf67b74626cf7e0fc537f7fb9
davidmungai <mungahdaudi@gmail.com>
2021-03-06 03:58:58 +0300

    Update delete_repo.sh

--- delete_repo.sh
+++ delete_repo.sh
@@ -3,5 +3,5 @@ input="./repos_list_deleting.txt"
 while IFS= read -r line
 do
  echo "$line"
- curl -X DELETE -H "Authorization: token 9a7f9caf1790c26b4122dfe0c614dcad01dc5dd1" "https://api.github.com/repos/$line";
+ curl -X DELETE -H "Authorization: token " "https://api.github.com/repos/$line";
 done < "$input"
